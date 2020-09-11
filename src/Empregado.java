59d600b20b2ea041ffa3f27489f0fa388a8449ba
angabba <aninha.bg21@yahoo.com.br>
2020-09-11 16:03:29 -0300

    exercicio 43

---
+++ src/Empregado.java
@@ -0,0 +1,16 @@
+public class Empregado {
+	
+	String nome;
+	String cargo;
+	double salario;
+	
+	void imprimir() {
+		System.out.println("**** FUNCIONARIA DO ANO ****");
+		System.out.println("Nome: "+nome);
+		System.out.println("Cargo: "+cargo);
+		System.out.println("Salario: "+salario);
+		System.out.println("___________________");
+		
+	}
+
+}
---
+++ src/EmpregadoTeste.java
@@ -0,0 +1,20 @@
+public class EmpregadoTeste {
+	public static void main(String args[]) {
+		
+		Empregado e1;
+		
+		e1 = new Empregado();
+		
+		e1.nome = "Ana";
+		e1.cargo = "Analista de produção";
+		e1.salario = 10000;
+		
+		e1.imprimir();
+		
+		
+	}
+	
+	
+	
+
+}
